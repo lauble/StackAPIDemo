eebe610c1f7cc3a4b9add11cc83ad58f49b25df8
Lauren <lauren.noble3@gmail.com>
2023-04-04 16:00:15 -0400

    added table styling and new parameters

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 async function getStackExchangeUsers() {
-  const apiUrl =
-    'https://api.stackexchange.com/2.3/users?order=desc&sort=reputation&site=stackoverflow';
+  const apiUrl = 'https://api.stackexchange.com/2.3/users?order=desc&sort=reputation&site=stackoverflow';
 
   // Make the API call
   const response = await fetch(apiUrl);
@@ -12,6 +11,12 @@ async function getStackExchangeUsers() {
     displayName: item.display_name,
     profileImage: item.profile_image,
     reputation: item.reputation,
+    badgeCounts: {
+      gold: item.badge_counts.gold,
+      silver: item.badge_counts.silver,
+      bronze: item.badge_counts.bronze,
+    },
+    location: item.location
   }));
   showTable(usersResponse);
 }
@@ -20,10 +25,15 @@ getStackExchangeUsers();
 
 const showTable = (data) => {
   let table = `<tr>
+    <th colspan=7>User Data</th>
+  </tr>
+  <tr>
     <th>account id</th>
     <th>display name</th>
     <th>profile image</th>
     <th>reputation</th>
+    <th>badge counts</th>
+    <th>location</th>
     </tr>`;
 
   // Loop to access all rows
@@ -32,7 +42,9 @@ const showTable = (data) => {
         <td>${item.accountId} </td>
         <td>${item.displayName}</td>
         <td><image width="200px" height="auto" src=${item.profileImage} /></td> 
-        <td>${item.reputation}</td>          
+        <td>${item.reputation}</td>
+        <td><b>gold:</b> ${item.badgeCounts.gold} <br/> silver: ${item.badgeCounts.silver} <br/> bronze: ${item.badgeCounts.bronze}</td>
+        <td>${item.location}</td>
         </tr>`;
   }
   document.getElementById('users').innerHTML = table;
